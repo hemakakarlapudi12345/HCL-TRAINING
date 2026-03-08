f8a5d9ccb2716711cdedd5eb8534375930123641
siva naga raju <nuvvun189@gmail.com>
2026-03-08 11:18:42 +0530

    Added student details program

---
+++ studentdetails.java
@@ -0,0 +1,13 @@
+class studentdetails {
+    public static void main(String[] args) {
+
+        String name = "Hema";
+        int rollNo = 19;
+        String course = "CSE";
+
+        System.out.println("Student Details");
+        System.out.println("Name: " + name);
+        System.out.println("Roll No: " + rollNo);
+        System.out.println("Course: " + course);
+    }
+}
