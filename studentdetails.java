0cad6dfa37fc01f0d681e1d5a498d2172e2e6cef
siva naga raju <nuvvun189@gmail.com>
2026-03-08 11:23:49 +0530

    Added age field to student details

--- studentdetails.java
+++ studentdetails.java
@@ -4,10 +4,12 @@ class studentdetails {
         String name = "Hema";
         int rollNo = 19;
         String course = "CSE";
+        int age = 21;
 
         System.out.println("Student Details");
         System.out.println("Name: " + name);
         System.out.println("Roll No: " + rollNo);
         System.out.println("Course: " + course);
+        System.out.println("Age: " + age);
     }
 }
